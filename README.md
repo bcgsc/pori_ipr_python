2eb06066ee0812abcd4700be71b3b37cae37f00b
Caralyn Reisle <creisle@bcgsc.ca>
2020-02-10 17:32:04 -0800

    Initial package setup

---
+++ README.md
@@ -0,0 +1,77 @@
+
+# Y
+
+
+
+
+## Getting Started
+
+### Install (For developers)
+
+clone this repository
+
+```
+git clone ssh://git@svn.bcgsc.ca:7999/sdev/genomic_report.git
+cd genomic_report
+```
+
+create a virtual environment
+
+```
+python3 -m venv venv
+source venv/bin/activate
+```
+
+install the package and its development dependencies
+
+```
+pip install -e .[dev]
+```
+
+Run the tests
+
+```
+pytest tests
+```
+
+## Deployment (Publishing)
+
+Install the deployment dependencies
+
+```bash
+pip install .[deploy]
+```
+
+Build the distribution files
+
+```bash
+python setup.py install sdist bdist_wheel
+```
+
+Upload the distibutions to the package server (-r defined in your pypirc)
+
+```bash
+twine upload -r bcgsc dist/*
+```
+
+## Deployment (Scripts)
+
+A buildout config is included by default which will build all console scripts defined
+in the package.
+
+create a virtual environment and install buildout
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -U pip setuptools zc.buildout
+```
+
+run buildout
+
+```bash
+buildout
+```
+
+This will create a directory `bin` with the executable scripts
+
---
+++ setup.py
@@ -0,0 +1,64 @@
+
+from setuptools import setup, find_packages
+
+# Dependencies required to use your package
+INSTALL_REQS = []
+
+# Dependencies required for development
+DEV_REQS = [
+    'flake8',
+    'black'
+]
+
+# Dependencies required only for running tests
+TEST_REQS = [
+    'pytest',
+    'pytest-cov'
+]
+
+# Dependencies required for deploying to an index server
+DEPLOYMENT_REQS = [
+    'twine',
+    'wheel',
+    'm2r'
+]
+
+long_description = ''
+long_description_content_type = 'text/markdown'
+
+try:
+    import m2r
+    import re
+
+    long_description = m2r.parse_from_file('README.md')
+    long_description = re.sub(
+        r'.. code-block::.*', '.. code::', long_description
+    )  # pyshop has issues with fenced code blocks
+    long_description_content_type = 'text/rst'
+except ImportError:
+    with open('README.md', 'r') as fh:
+        long_description = fh.read()
+
+
+setup(
+    name='y',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=INSTALL_REQS,
+    extras_require={
+        'dev': TEST_REQS + DEPLOYMENT_REQS + DEV_REQS,
+        'deploy': DEPLOYMENT_REQS,
+        'test': TEST_REQS
+    },
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    python_requires='>=3',
+    author_email='creisle',
+    author='creisle@bcgsc.ca',
+    maintainer='creisle',
+    maintainer_email='creisle@bcgsc.ca',
+    dependency_links=[],
+    test_suite='tests',
+    tests_require=TEST_REQS,
+    entry_points={'console_scripts': []}
+)
