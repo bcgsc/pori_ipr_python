c506e219b876321c59b88770aad80ff2bc7f0f50
Caralyn Reisle <creisle@bcgsc.ca>
2020-02-19 16:40:09 -0800

    Lint setup and add entry point

--- setup.py
+++ setup.py
@@ -1,27 +1,16 @@
-
 from setuptools import setup, find_packages
 
 # Dependencies required to use your package
-INSTALL_REQS = []
+INSTALL_REQS = ['graphkb==0.1.0']
 
 # Dependencies required for development
-DEV_REQS = [
-    'flake8',
-    'black'
-]
+DEV_REQS = ['flake8', 'black']
 
 # Dependencies required only for running tests
-TEST_REQS = [
-    'pytest',
-    'pytest-cov'
-]
+TEST_REQS = ['pytest', 'pytest-cov']
 
 # Dependencies required for deploying to an index server
-DEPLOYMENT_REQS = [
-    'twine',
-    'wheel',
-    'm2r'
-]
+DEPLOYMENT_REQS = ['twine', 'wheel', 'm2r']
 
 long_description = ''
 long_description_content_type = 'text/markdown'
@@ -48,7 +37,7 @@ setup(
     extras_require={
         'dev': TEST_REQS + DEPLOYMENT_REQS + DEV_REQS,
         'deploy': DEPLOYMENT_REQS,
-        'test': TEST_REQS
+        'test': TEST_REQS,
     },
     long_description=long_description,
     long_description_content_type=long_description_content_type,
@@ -60,5 +49,5 @@ setup(
     dependency_links=[],
     test_suite='tests',
     tests_require=TEST_REQS,
-    entry_points={'console_scripts': []}
+    entry_points={'console_scripts': ['genomic_report = genomic_report.main:main']},
 )
