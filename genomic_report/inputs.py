8fc60852cfb870ab56a6a7b69d396277970d94fe
Caralyn Reisle <creisle@bcgsc.ca>
2020-02-21 18:03:42 -0800

    Add function to check variant links

--- genomic_report/inputs.py
+++ genomic_report/inputs.py
@@ -4,6 +4,13 @@ Read/Validate the variant input files
 from csv import DictReader
 import re
 
+from graphkb.match import INPUT_COPY_CATEGORIES, INPUT_EXPRESSION_CATEGORIES
+from Bio.Data.IUPACData import protein_letters_3to1
+
+from .util import hash_key
+
+protein_letters_3to1.setdefault('Ter', '*')
+
 
 def load_variant_file(filename, required, optional, row_to_key):
     """
@@ -144,6 +151,22 @@ def load_expression_variants(filename):
         ],
         row_key,
     )
+    patterns = {
+        'expression_class': r'^(overexpressed|outlier_high|outlier_low|low_percentile|na|)$'
+    }
+
+    validate_row_patterns(result, patterns)
+
+    # TODO: transform expression class to 'variant' column of GraphKB vocabulary
+    for row in result:
+        variant = ''
+
+        if row['expression_class'] in {'outlier_low', 'low_percentile'}:
+            variant = INPUT_EXPRESSION_CATEGORIES.DOWN
+        elif row['expression_class'] in {'outlier_high', 'overexpressed'}:
+            variant = INPUT_EXPRESSION_CATEGORIES.UP
+
+        row['variant'] = variant
 
     return result
 
@@ -175,3 +198,59 @@ def load_structural_variants(filename):
     validate_row_patterns(result, patterns)
 
     return result
+
+
+def check_variant_links(small_mutations, expression_variants, copy_variants, structural_variants):
+    """
+    Check that there is matching expression and copy variant information for any genes with variants
+
+    Args:
+        small_mutations (list.<dict>): list of small mutations
+        expression_variants (list.<dict>): list of expression variants
+        copy_variants (list.<dict>): list of copy variants
+        structural_variants (list.<dict>): list of structural variants
+
+    Raises:
+        KeyError: A variant is called on a gene without expression or without copy number information
+
+    Returns:
+        set.<str>: set of gene names with variants (used for filtering before upload to IPR)
+    """
+    # filter excess variants not required for extra gene information
+    copy_variant_genes = {variant['gene'] for variant in copy_variants}
+    expression_variant_genes = {variant['gene'] for variant in expression_variants}
+    genes_with_variants = set()  # filter excess copy variants
+
+    for variant in copy_variants:
+        gene = variant['gene']
+        if variant['variant']:
+            genes_with_variants.add(gene)
+
+            if gene not in expression_variant_genes:
+                raise KeyError(
+                    f'gene ({gene}) has a copy variant but is missing expression information'
+                )
+
+    for variant in expression_variants:
+        gene = variant['gene']
+        if variant['variant']:
+            genes_with_variants.add(gene)
+
+            if gene not in copy_variant_genes:
+                raise KeyError(
+                    f'gene ({gene}) has an expression variant but is missing copy number information'
+                )
+
+    for variant in small_mutations:
+        gene = variant['gene']
+        if gene not in copy_variant_genes:
+            raise KeyError(
+                f'gene ({gene}) has a small mutation but is missing copy number information'
+            )
+        if gene not in expression_variant_genes:
+            raise KeyError(
+                f'gene ({gene}) has a small mutation but is missing expression information'
+            )
+        genes_with_variants.add(gene)
+
+    return genes_with_variants
--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -1,6 +1,13 @@
 import os
 
-from genomic_report.inputs import load_small_mutations, load_copy_variants
+import pytest
+
+from genomic_report.inputs import (
+    load_small_mutations,
+    load_copy_variants,
+    check_variant_links,
+    load_expression_variants,
+)
 
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'test_data')
@@ -16,3 +23,74 @@ def test_load_copy_variants():
     records = load_copy_variants(os.path.join(DATA_DIR, 'copy_variants.tab'))
     assert records
     assert len(records) == 4599
+
+
+@pytest.mark.skip('TODO')
+def test_load_structural_variants():
+    pass
+
+
+def test_load_expression_variants():
+    records = load_expression_variants(os.path.join(DATA_DIR, 'expression.tab'))
+    assert records
+    assert len(records) == 6
+
+
+class TestCheckVariantLinks:
+    def test_sm_missing_copy(self):
+        with pytest.raises(KeyError):
+            check_variant_links(
+                small_mutations=[{'gene': 'KRAS'}],
+                copy_variants=[],
+                expression_variants=[{'gene': 'KRAS', 'variant': ''}],
+                structural_variants=[],
+            )
+
+    def test_sm_missing_exp(self):
+        with pytest.raises(KeyError):
+            check_variant_links(
+                small_mutations=[{'gene': 'KRAS'}],
+                copy_variants=[{'gene': 'KRAS', 'variant': ''}],
+                expression_variants=[],
+                structural_variants=[],
+            )
+
+    @pytest.mark.skip('TODO')
+    def test_sv_missing_copy(self):
+        with pytest.raises(KeyError):
+            pass
+
+    @pytest.mark.skip('TODO')
+    def test_sv_missing_exp(self):
+        with pytest.raises(KeyError):
+            pass
+
+    def test_with_valid_inputs(self):
+        genes = check_variant_links(
+            small_mutations=[{'gene': 'KRAS'}],
+            copy_variants=[{'gene': 'KRAS', 'variant': ''}, {'gene': 'CDK', 'variant': ''}],
+            expression_variants=[{'gene': 'KRAS', 'variant': ''}],
+            structural_variants=[],
+        )
+        assert genes == {'KRAS'}
+
+    def test_copy_missing_exp(self):
+        with pytest.raises(KeyError):
+            check_variant_links(
+                small_mutations=[],
+                copy_variants=[
+                    {'gene': 'BRAF', 'variant': 'copy gain'},
+                    {'gene': 'KRAS', 'variant': ''},
+                ],
+                expression_variants=[{'gene': 'KRAS', 'variant': ''}],
+                structural_variants=[],
+            )
+
+    def test_exp_missing_copy(self):
+        with pytest.raises(KeyError):
+            check_variant_links(
+                small_mutations=[],
+                copy_variants=[{'gene': 'KRAS', 'variant': ''}],
+                expression_variants=[{'gene': 'BRAF', 'variant': 'increased expression'}],
+                structural_variants=[],
+            )
