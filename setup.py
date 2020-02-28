bb402cb5fd7e0906746d236e80a84c52a0b8d90d
Caralyn Reisle <creisle@bcgsc.ca>
2020-02-28 14:00:10 -0800

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ except ImportError:
 
 setup(
     name='genomic_report',
-    version='0.1.1',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=INSTALL_REQS,
     extras_require={
