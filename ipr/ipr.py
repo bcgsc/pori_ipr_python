cb0dafaad979daefdd0bb7566767c0930157efc2
Dustin Bleile <dbleile@bcgsc.ca>
2022-06-07 13:13:58 -0700

    GERO-283 - pori_ipr_python/pull/78 - fix germline filter logging messages & assume_somatic filter.

--- ipr/ipr.py
+++ ipr/ipr.py
@@ -267,21 +267,29 @@ def germline_kb_matches(
         for alt in germ_alts:
             var_list = [v for v in all_variants if v['key'] == alt['variant']]
             germline_var_list = [v for v in var_list if 'germline' in v and v['germline']]
+            unknown_var_list = [v for v in var_list if 'germline' not in v]
             if germline_var_list:
                 logger.debug(
                     f"germline kbStatementId:{alt['kbStatementId']}: {alt['kbVariant']} {alt['category']}"
                 )
                 ret_list.append(alt)
-            elif var_list:
+            elif unknown_var_list:
+                logger.warning(
+                    f"germline no data fail for: {alt['kbStatementId']}: {alt['kbVariant']} {alt['category']}"
+                )
+                if not assume_somatic:
+                    logger.debug(
+                        f"Keeping unverified match to germline kbStatementId:{alt['kbStatementId']}: {alt['kbVariant']} {alt['category']}"
+                    )
+                    ret_list.append(alt)
+                else:
+                    logger.debug(
+                        f"Dropping unverified match to germline kbStatementId:{alt['kbStatementId']}: {alt['kbVariant']} {alt['category']}"
+                    )
+            else:
                 logger.debug(
                     f"Dropping somatic match to germline kbStatementId:{alt['kbStatementId']}: {alt['kbVariant']} {alt['category']}"
                 )
-            else:  # no data
-                logger.warning(
-                    f"germline check fail for: {alt['kbStatementId']}: {alt['kbVariant']} {alt['category']}"
-                )
-                if not assume_somatic:
-                    ret_list.append(alt)
     if somatic_alts:
         # Remove any matches to germline events
         for alt in somatic_alts:
@@ -290,7 +298,7 @@ def germline_kb_matches(
             if somatic_var_list:
                 ret_list.append(alt)
             else:
-                logger.info(
+                logger.debug(
                     f"Dropping germline match to somatic statement kbStatementId:{alt['kbStatementId']}: {alt['kbVariant']} {alt['category']}"
                 )
 
