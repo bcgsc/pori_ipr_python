7812a8255657ac1550cfa66c2ed0708c8f5aa04f
Dustin Bleile <dbleile@bcgsc.ca>
2023-04-12 14:31:11 -0700

    GERO-318 - just use IPR-A evidence equivalence as approved therapies.  Remove older, outdated references to approved therapies.

--- ipr/constants.py
+++ ipr/constants.py
@@ -1,21 +1,7 @@
-GERMLINE_BASE_TERMS = ('pharmacogenomic', 'cancer predisposition')  # based on graphkb.constants
-VARIANT_CLASSES = {'Variant', 'CategoryVariant', 'PositionalVariant', 'CatalogueVariant'}
-
-
-APPROVED_EVIDENCE_LEVELS = {
-    # sourceIds of levels by source name
-    'oncokb': ['1', 'r1'],
-    'profyle': ['t1'],
-    'cancer genome interpreter': [
-        'cpic guidelines',
-        'european leukemianet guidelines',
-        'fda guidelines',
-        'nccn guidelines',
-        'nccn/cap guidelines',
-    ],
-}
 
 DEFAULT_URL = 'https://iprstaging-api.bcgsc.ca/api'
+GERMLINE_BASE_TERMS = ('pharmacogenomic', 'cancer predisposition')  # based on graphkb.constants
+VARIANT_CLASSES = {'Variant', 'CategoryVariant', 'PositionalVariant', 'CatalogueVariant'}
 
 # all possible values for review status are: ['pending', 'not required', 'passed', 'failed', 'initial']
 FAILED_REVIEW_STATUS = 'failed'
